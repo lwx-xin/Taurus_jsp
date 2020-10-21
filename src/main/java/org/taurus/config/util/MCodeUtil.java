1ac2608073184ae8396e0eb283103c01dc41909d
lwx-xin <2697434967@qq.com>
2020-10-21 13:36:55 +0800

    为方法(getGroupMap)添加过滤已删除的数据以及按order排序

--- src/main/java/org/taurus/config/util/MCodeUtil.java
+++ src/main/java/org/taurus/config/util/MCodeUtil.java
@@ -1,8 +1,10 @@
 package org.taurus.config.util;
 
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.taurus.config.LoadCodeData;
 import org.taurus.entity.sys.TMCodeEntity;
@@ -43,9 +45,16 @@ public class MCodeUtil {
 		Map<String, List<TMCodeEntity>> sys_mCode = LoadCodeData.tmCode;
 		List<TMCodeEntity> list = sys_mCode.get(group);
 		
+		List<TMCodeEntity> sortList = list.stream().sorted(Comparator.comparing(TMCodeEntity::getCodeOrder)).collect(Collectors.toList());
+		
 		Map<String, String> map = new HashMap<String, String>();
-		if (ListUtil.isNotEmpty(list)) {
-			for (TMCodeEntity codeEntity : list) {
+		if (ListUtil.isNotEmpty(sortList)) {
+			for (TMCodeEntity codeEntity : sortList) {
+				String delFlg = codeEntity.getCodeDelFlg();
+				if (CodeKeyValue.DEL_FLG_YES.value().equals(delFlg)) {
+					continue;
+				}
+				
 				String name = codeEntity.getCodeName();
 				String value = codeEntity.getCodeValue();
 				map.put(value, name);
