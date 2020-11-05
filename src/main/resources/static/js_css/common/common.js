71009e1c36724f269b7f8d90e691df185c0b595b
lwx-xin <2697434967@qq.com>
2020-11-05 13:05:07 +0800

    移除移动端、pc端检测

--- src/main/resources/static/js_css/common/common.js
+++ src/main/resources/static/js_css/common/common.js
@@ -10,6 +10,22 @@ document.oncontextmenu = function(){
     }
 };*///禁止F12
 
+
+//设备检测-移动端返回true，pc端返回false
+function isMobile() {
+	if( navigator.userAgent.match(/Android/i)
+		|| navigator.userAgent.match(/webOS/i)
+		|| navigator.userAgent.match(/iPhone/i)
+		|| navigator.userAgent.match(/iPad/i)
+		|| navigator.userAgent.match(/iPod/i)
+		|| navigator.userAgent.match(/BlackBerry/i)
+		|| navigator.userAgent.match(/Windows Phone/i)
+	){
+		return true;
+	}
+	return false;
+}
+
 /**
 * 使用时候直接调用方法
 * Format(date,"yyyy-MM-dd HH:mm");输出格式为 "2015-10-14 16:50"；
